254b62db1f5105b199f0d20b895cacf15e0ff6c3
Medina <medina-dadurgova@hotmail.com>
2022-09-03 20:27:03 +0200

    styling

--- script/app.js
+++ script/app.js
@@ -2,27 +2,37 @@
 const rarityValue = document.querySelector('#rarity h1');
 const rarityGenerator = document.querySelector('#rarity button');
 const generatedCard = document.querySelector('#generatedCard');
-const buttons = document.querySelectorAll('#generateCard button');
+const buttons = document.querySelectorAll('.buttons button');
 const cardOutput = document.querySelector('#selectedCard h1');
 let cardType;
+let dataset;
+let deckName;
+
+const date = new Date();
+document.getElementById('year').innerText = date.getFullYear();
 
 randomizeCardType();
 
 function randomizeCardType() {
     let rarity = Math.floor((Math.random() * 100) + 1);
 
+    document.querySelector('.deck').hidden = true;
+    document.querySelector('.deck').innerText = "";
+
     if (rarity === 1) {
-        cardType = "legendary";
+        cardType = "limited";
+        document.getElementById('rarity').style.background = "linear-gradient(to right,rgb(222, 168, 248) 10%,rgb(168, 222, 258) 30%,rgb(189, 250, 205) 42%,rgb(243, 250, 189) 58%,rgb(250, 227, 189) 70%,rgb(248, 172, 171) 95%,rgb(254, 170, 212) 100%)";
     } else if (rarity > 1 && rarity < 21) {
         cardType = "rare";
+        document.getElementById('rarity').style.background = "linear-gradient(to right, #BF953F, #FCF6BA)";
     } else {
         cardType = "common";
+        document.getElementById('rarity').style.background = "#9f5bcf";
     }
 
     rerollRarity();
 
     rarityValue.innerText = `${cardType} ${rarity}`;
-    console.log(rarity);
 }
 
 function rerollRarity() {
@@ -33,20 +43,24 @@ function rerollRarity() {
 
 for (let button of buttons) {
     error.hidden = true;
+    rarityValue.hidden = false;
     button.onclick = function () {
         fetchData(button);
     }
 }
 
 async function fetchData(button) {
-    const response = await fetch(`resources/${button.dataset.deck}.json`);
+    dataset = button.dataset.deck;
+    deckName = button.innerText;
+    const response = await fetch(`resources/${dataset}.json`);
     if (response.ok) {
         const cards = await response.json();
         printCards(cards);
     } else {
         const error = document.getElementById('error');
         error.hidden = false;
-        error.style.backgroundColor = 'red';
+        document.getElementById('rarity').style.background = "#e03030";
+        rarityValue.hidden = true;
     }
 }
 
@@ -54,5 +68,9 @@ function printCards(cards) {
     let randomCard = Math.floor((Math.random() * cards.length));
     console.log(`${cards[randomCard].group} ${cards[randomCard].name}`);
     console.log(randomCard);
-    cardOutput.innerText = `${cards[randomCard].group} ${cards[randomCard].name}`;
+    console.log(deckName);
+    rarityValue.innerText = `${cards[randomCard].group} ${cards[randomCard].name}`;
+    document.getElementById('rarity').style.background = "#04b486";
+    document.querySelector('.deck').hidden = false;
+    document.querySelector('.deck').innerText = deckName;
 }
